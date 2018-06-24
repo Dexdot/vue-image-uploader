fb5d2ea04c915a8722052ce510afc0e3ddc2d0af
Dexdot <dexdotapb@gmail.com>
2018-06-24 14:26:20 +0300

    Gallery styling

--- src/components/ImageList.vue
+++ src/components/ImageList.vue
@@ -1,9 +1,12 @@
 <template>
   <div>
     <h1>Image List</h1>
-    <div>
-      <img v-for="img in allImages" :key="img.id" :src="img.link" :alt="img.title">
+    <div v-if="isLoggedIn">
+      <div class="image-container">
+        <img v-for="img in allImages" :key="img.id" :src="img.link" :alt="img.title">
+      </div>
     </div>
+    <h2 v-else>Please, login to get started!</h2>
   </div>
 </template>
 
@@ -13,10 +16,20 @@ import { mapActions, mapGetters } from 'vuex';
 export default {
   name: 'ImageList',
   methods: mapActions(['fetchImages']),
-  computed: mapGetters(['allImages']),
+  computed: mapGetters(['allImages', 'isLoggedIn']),
   created() {
     this.fetchImages();
   }
 };
 </script>
 
+<style scoped>
+.image-container {
+  column-count: 3;
+  column-gap: 0;
+}
+img {
+  max-width: 100%;
+  padding: 5px;
+}
+</style>
